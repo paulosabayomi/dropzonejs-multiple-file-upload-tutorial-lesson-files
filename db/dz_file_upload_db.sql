d674f7ca99516414db8ee83a2abc06a0ec63fe93
Amusa Abayomi <amusaabayomipaul99@gmail.com>
2021-01-13 19:18:59 +0100

    the lesson files of Multiple file upload with Dropzonejs in jQuery/AJAX, PHP and MySQL database.

---
+++ db/dz_file_upload_db.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 13, 2021 at 06:15 PM
+-- Server version: 8.0.21
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dz_file_upload_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE IF NOT EXISTS `files` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `filename` varchar(300) NOT NULL,
+  `parentFolder` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ db_request.php
@@ -0,0 +1,57 @@
+<?php
+
+$host = "localhost";
+$db = "dz_file_upload_db";
+$user = "root";
+$pwd = "";
+
+$conn = mysqli_connect($host, $user, $pwd, $db);
+
+if (!$conn) {
+    die("Could not connect to the database because: ". mysqli_error($conn));
+}
+
+$file = $_FILES["file"];
+
+$fileName = $file["name"]; // the name of the file
+
+$fileType = $file["type"]; // the file type
+
+$fileTmpName = $file["tmp_name"]; // the temp name
+
+$fileError = $file["error"]; // to check if any error ocurred or not - 1 or 0
+
+$fileSize = $file["size"]; // the file size
+
+$fileExt = explode(".", $fileName); // splitted the filename and the file extension
+
+$year = date("Y", strtotime("now"));
+
+$month = date("m", strtotime("now"));
+
+$mainLocation = "uploads/";
+
+$parentsLocation = $year . "/" . $month . "/";
+
+$folder = $mainLocation . $parentsLocation;
+
+if (!file_exists($folder)) {
+    mkdir($folder, 0777, true);
+}
+
+$realFilename = uniqid($fileExt[0]) . "." . end($fileExt);
+$filePermLoc = $folder . $realFilename;
+if(move_uploaded_file($fileTmpName, $filePermLoc)){
+    $sql = "INSERT INTO files (filename, parentFolder)VALUES(?,?)";
+    $stmt = mysqli_stmt_init($conn);
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "ss", $realFilename, $parentsLocation);
+
+    mysqli_stmt_execute($stmt);
+    echo json_encode(["status"=>"0", "msg"=>"the file " . $fileName . " uploaded successfully!"]);
+} else {
+    echo json_encode(["status"=>"1", "msg"=>"the file " . $fileName . " couldn't be uploaded due to some issues"]);
+}
+
+
+
