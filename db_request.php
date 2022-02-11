6938ce6ecd2da9edaaeba3c87028c5fcf05e86d9
Hostel Rentie <hostelrentie@gmail.com>
2022-02-11 18:14:17 +0100

    added uncommited changes

--- db_request.php
+++ db_request.php
@@ -1,23 +1,28 @@
 <?php
 
-$host = "localhost";
-$db = "dz_file_upload_db";
-$user = "root";
-$pwd = "";
 
-$conn = mysqli_connect($host, $user, $pwd, $db);
 
-if (!$conn) {
-    die("Could not connect to the database because: ". mysqli_error($conn));
-}
+// $host = "localhost";
+// $db = "dz_file_upload_db";
+// $user = "root";
+// $pwd = "";
+
+// $conn = mysqli_connect($host, $user, $pwd, $db);
+
+// if (!$conn) {
+//     die("Could not connect to the database because: ". mysqli_error($conn));
+// }
 
 $file = $_FILES["file"];
+
 
 $fileName = $file["name"]; // the name of the file
 
 $fileType = $file["type"]; // the file type
 
 $fileTmpName = $file["tmp_name"]; // the temp name
+echo explode("/", mime_content_type($fileTmpName))[0];
+die();
 
 $fileError = $file["error"]; // to check if any error ocurred or not - 1 or 0
 
